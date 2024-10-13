a9f22d390cd695553e5800dc8f34c83cb46b7d7b
Tomoaki Yagishita <tyagishi@gmail.com>
2024-10-13 15:05:41 +0900

    F - remove unused code

--- RefFileBundleDocApp/RefFileBundleDocument.swift
+++ RefFileBundleDocApp/RefFileBundleDocument.swift
@@ -27,9 +27,6 @@ class RefFileBundleDocument: ReferenceFileDocument {
     static let text1Key = "Text1Key.txt"
     static let text2Key = "Text2Key.txt"
 
-//    var text1: String = "Text1"
-//    var text2: String = "Text2"
-//
     var rootNode: TreeNode<FileSystemItem>
     
     init() {
@@ -38,11 +35,6 @@ class RefFileBundleDocument: ReferenceFileDocument {
         rootNode = TreeNode(value: .init(directory: "root", nil))
         rootNode.addChildwithFileWrapper(TreeNode(value: .init(filename: Self.text1Key, text: "HelloText1", nil)))
         rootNode.addChildwithFileWrapper(TreeNode(value: .init(filename: Self.text2Key, text: "WorldText2", nil)))
-
-//        let text1FileWrapper = FileWrapper(regularFileWithContents: text1.data(using: .utf8)!)
-//        let text2FileWrapper = FileWrapper(regularFileWithContents: text2.data(using: .utf8)!)
-//        self.rootFileWrapper = FileWrapper(directoryWithFileWrappers: [Self.text1Key: text1FileWrapper,
-//                                                                       Self.text2Key: text2FileWrapper])
     }
 
     required init(configuration: ReadConfiguration) throws {
@@ -62,22 +54,6 @@ class RefFileBundleDocument: ReferenceFileDocument {
                   let text = String(data: textData, encoding: .utf8) else { continue }
             rootNode.addChild(TreeNode(value: .init(filename: key, text: text, childFileWrapper)))
         }
-
-//        // check structure
-//        guard rootFileWrapper.isDirectory,
-//              let text1FileWrapper = rootFileWrapper.fileWrappers?[Self.text1Key],
-//              let text2FileWrapper = rootFileWrapper.fileWrappers?[Self.text2Key] else { return }//fatalError("unknown document") }
-//        self.rootFileWrapper = rootFileWrapper
-//        rootFileWrapper.addFileWrapper(text1FileWrapper)
-//        rootFileWrapper.addFileWrapper(text2FileWrapper)
-//
-//        // extract data from filewrapper
-//        guard let text1Data = text1FileWrapper.regularFileContents,
-//              let text1 = String(data: text1Data, encoding: .utf8),
-//              let text2Data = text2FileWrapper.regularFileContents,
-//              let text2 = String(data: text2Data, encoding: .utf8) else { fatalError("strange document content") }
-//        self.text1 = text1
-//        self.text2 = text2
     }
 
     func snapshot(contentType: UTType) throws -> TreeNode<FileSystemItem> {
@@ -101,24 +77,5 @@ class RefFileBundleDocument: ReferenceFileDocument {
             }
         }
         return rootFileWrapper
-
-//        if rootFileWrapper == nil {
-//            rootFileWrapper = FileWrapper(directoryWithFileWrappers: [:])
-//        }
-//        guard let rootFileWrapper = rootFileWrapper else { fatalError("failed to create root directory")}
-//
-//        if let text1FileWrapper = rootFileWrapper.fileWrappers?[Self.text1Key],
-//           text1FileWrapper.regularFileContents != snapshot[0].data(using: .utf8) {
-//            print("update filewrapper1")
-//            rootFileWrapper.removeFileWrapper(text1FileWrapper)
-//            rootFileWrapper.addRegularFile(withContents: snapshot[0].data(using: .utf8)!, preferredFilename: Self.text1Key)
-//        }
-//
-//        if let text2FileWrapper = rootFileWrapper.fileWrappers?[Self.text2Key],
-//           text2FileWrapper.regularFileContents != snapshot[1].data(using: .utf8) {
-//            print("update filewrapper2")
-//            rootFileWrapper.removeFileWrapper(text2FileWrapper)
-//            rootFileWrapper.addRegularFile(withContents: snapshot[1].data(using: .utf8)!, preferredFilename: Self.text2Key)
-//        }
     }
 }
